090104917767da8cdf33ab6e5241b958450e900c
Ho-Sheng Hsiao <hosh@sparkfly.com>
2010-04-07 02:09:17 -0400

    [PGSQL] PostgreSQL Adapter when extracting foreign keys from a table should extract single foreign key

--- spec/postgresql/schema_extractor_spec.rb
+++ spec/postgresql/schema_extractor_spec.rb
@@ -9,7 +9,22 @@ describe Foreigner::ConnectionAdapters::PostgreSQLAdapter do
   end
 
   describe 'when extracting foreign keys from a table' do 
-    it 'should extract single foreign key' 
+    it 'should extract single foreign key'  do
+      create_table :items do |t|
+        t.string :name
+        t.references :collection, :null => false
+        t.foreign_key :collection
+      end
+
+      @adapter.foreign_keys(:items).length.should eql(1)
+      foreign_key = @adapter.foreign_keys(:items)[0]
+
+      # Duck Typing
+      foreign_key.should be_respond_to(:from_table)
+      foreign_key.should be_respond_to(:to_table)
+      foreign_key.should be_respond_to(:options)
+    end
+
     it 'should extract multiple foreign keys'
     it 'should extract referencing table'
     it 'should extract foreign table'
